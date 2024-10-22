050ed66e79690fbc631985b855c36328368a61be
sreeharipremanathan <sreeharipremanathan@gmail.com>
2024-10-22 13:50:01 +0530

    sms

---
+++ student_manage_system/app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Student(models.Model):
+    roll_no=models.IntegerField
+    # name=models.CharField(max_length=(25))
+    name=models.TextField
+    age=models.IntegerField
+    email=models.EmailField
+    phn=models.IntegerField
