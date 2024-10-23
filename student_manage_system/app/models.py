344640803835cb81c0a6d127c07f0c04a9c63779
sreeharipremanathan <sreeharipremanathan@gmail.com>
2024-10-23 13:46:52 +0530

    Add files via upload
    sms

--- student_manage_system/app/models.py
+++ student_manage_system/app/models.py
@@ -3,9 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
-    roll_no=models.IntegerField
+    roll_no=models.IntegerField()
     # name=models.CharField(max_length=(25))
-    name=models.TextField
-    age=models.IntegerField
-    email=models.EmailField
-    phn=models.IntegerField
+    name=models.TextField()
+    age=models.IntegerField()
+    email=models.EmailField()
+    phn=models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- student_manage_system/app/views.py
+++ student_manage_system/app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+def display_std(req):
+    data=Student.objects.all()
+    return render(req,'display_std.html',{"data":data})
