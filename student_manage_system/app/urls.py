77432e7a6600fe48e59ed9fd5e09bbe7722fc79c
sreeharipremanathan <sreeharipremanathan@gmail.com>
2024-10-24 13:38:47 +0530

    Add files via upload
    sms

--- student_manage_system/app/urls.py
+++ student_manage_system/app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('dis',views.display_std)
+    path('dis',views.display_std),
+    path('add_std',views.add),
+    path('edit_std/<id>',views.edit)
 ]
--- student_manage_system/app/views.py
+++ student_manage_system/app/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
 # Create your views here.
 def display_std(req):
     data=Student.objects.all()
     return render(req,'display_std.html',{"data":data})
+
+def add(req):
+    if req.method=='POST':
+        roll=req.POST["roll"]
+        std_name=req.POST["name"]
+        std_age=req.POST["age"]
+        std_email=req.POST["email"]
+        std_phno=req.POST["phno"]
+        data=Student.objects.create(roll_no=roll,name=std_name,age=std_age,email=std_email,phn=std_phno)
+        data.save()
+        return redirect(display_std)
+    else:
+        return redirect(display_std)
+    
+def edit(req,id):
+    data=Student.objects.get(pk=id)
+    if req.method=='POST':
+        roll=req.POST["roll"]
+        std_name=req.POST["name"]
+        std_age=req.POST["age"]
+        std_email=req.POST["email"]
+        std_phno=req.POST["phno"]
+        Student.objects.filter(pk=id).update(roll_no=roll,nammse=std_name,age=std_age,email=std_email,phn=std_phno)
+        return redirect(display_std)
+    else:
+        return render(req,'edit.html',{'data':data})
