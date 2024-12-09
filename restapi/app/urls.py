3b2a0decc32c49de1b5b96ab3332069fe5bb9598
sreeharipremanathan <sreeharipremanathan@gmail.com>
2024-12-09 23:25:57 +0530

    restapi

--- restapi/app/urls.py
+++ restapi/app/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns=[
     path('fun1',views.fun1),
     path('fun2',views.fun2),
+    path('fun3/<d>',views.fun3),
+    path('fun4',views.fun4),
 ]
--- restapi/app/views.py
+++ restapi/app/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render,redirect
 from .models import *
 from .serializers import *
 from django.http import JsonResponse
+from django.http import HttpResponse
 from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework import generics,mixins
 # Create your views here.
 
 def fun1(req):
@@ -11,6 +18,8 @@ def fun1(req):
         s=Sample(d,many=True)
         return JsonResponse(s.data,safe=False)
     
+
+@csrf_exempt    
 def fun2(req):
     if req.method=='GET':
         d=Student.objects.all()
@@ -24,3 +33,41 @@ def fun2(req):
             return JsonResponse(s.data)
         else:
             return JsonResponse(s.errors)
+        
+
+@csrf_exempt
+def fun3(req,d):
+    try:
+        demo=Student.objects.get(pk=d)
+    except Student.DoesNotExist:
+        return HttpResponse('invalid')
+    if req.method=='GET':
+        s=model_serializers(demo)
+        return JsonResponse(s.data)
+    elif req.method=='PUT':
+        d=JSONParser().parse(req)
+        s=model_serializers(demo,data=d)
+        if s.is_valid():
+            s.save()
+            return JsonResponse(s.data)
+        else:
+            return JsonResponse(s.errors)
+    elif req.method=="DELETE":
+        demo.delete()
+        return HttpResponse('deleted')
+    
+@api_view(['GET','POST'])
+def fun4(req):
+    if req.method=='GET':
+        d=Student.objects.all()
+        s=model_serializers(d,many=True)
+        return Response(s.data)
+    
+    elif req.method=='POST':
+        s=model_serializers(data=req.data)
+        if s.is_valid():
+            s.save()
+            return JsonResponse(s.data,status=status.HTTP_201_CREATED)
+        else:
+            return JsonResponse(s.errors,status=status.HTTP_400_BAD_REQUEST)
+        
